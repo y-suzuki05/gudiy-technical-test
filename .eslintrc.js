65f40f09748a9633e971c292d8127ec268271e77
yuki <momo_968_weekend@yahoo.co.jp>
2024-10-27 18:52:04 +0900

    ESLint、Prettierの設定とその実行

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: [
+    'next/core-web-vitals',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jsx-a11y/recommended',
+    'prettier',
+  ]
+}
