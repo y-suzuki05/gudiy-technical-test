93cffefe07ec8df26396900c34c252ce6be3979e
yuki <momo_968_weekend@yahoo.co.jp>
2024-10-27 18:57:26 +0900

    typescript-eslintの設定

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,16 @@ module.exports = {
     'next/core-web-vitals',
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
     'plugin:jsx-a11y/recommended',
     'prettier',
-  ]
+  ],
+  "parser": "@typescript-eslint/parser",
+  "parserOptions":  {
+    "project": "./tsconfig.json"
+  },
+  "plugins": [
+    "@typescript-eslint"
+  ],
+  "root": true
 }
